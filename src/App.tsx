8472746c028e58d5e06ad94c65412938b4791060
Artem <plzv92@gmail.com>
2024-02-28 12:29:52 +0700

    add: footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import { TableView } from "@/components/table-view/table-view";
 import { Button } from "@/components/ui/button";
 import { Header } from "@/components/header/header";
 
-import { ArrowUp } from "lucide-react";
+import { ArrowUp, Link } from "lucide-react";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 const view = {
   table: <TableView />,
@@ -37,6 +38,20 @@ const App = () => {
       >
         <ArrowUp />
       </Button>
+
+      <div className="h-40 bg-slate-50 mt-10 flex justify-center flex-col items-center">
+        <a
+          href="https://github.com/hypnosisflow/rickandmortywiki"
+          target="_blank"
+          className="flex items-center gap-2"
+        >
+          <span>Source code</span>
+          <GitHubLogoIcon />
+        </a>
+        <p className="text-center text-slate-300 text-xs">
+          hypnosisflow @ 2024
+        </p>
+      </div>
     </>
   );
 };
